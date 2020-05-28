74552cd8a5de477083389b958c03dddfe532c3ad
Juan Cassiano <juancassiano@hotmail.com>
2020-05-27 22:02:03 -0300

    Add context and save in localstorage

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,20 +1,29 @@
-import React, { useRef, useCallback } from 'react';
+import React, { useRef, useCallback, useContext } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 import logoImg from '../../assets/logo.svg';
 
+import { AuthContext } from '../../context/AuthContext';
 import getValidationErrors from '../../utils/getValidationErrors';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
+interface SignInFormData {
+  email: string;
+  password: string;
+}
+
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const handleSubmit = useCallback(async (data: object) => {
+  const { user, signIn } = useContext(AuthContext);
+
+  console.log(user);
+  const handleSubmit = useCallback(async (data: SignInFormData) => {
    try{
     formRef.current?.setErrors({});
 
@@ -29,12 +38,17 @@ const SignIn: React.FC = () => {
     await schema.validate(data,{
       abortEarly: false,
     });
+
+    signIn({
+      email: data.email,
+      password: data.password,
+    });
     }catch (err) {
       const errors = getValidationErrors(err);
 
       formRef.current?.setErrors(errors);
     }
-  },[]);
+  },[signIn]);
 
   return(
     <Container>

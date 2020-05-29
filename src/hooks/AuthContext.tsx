a748b7a8915ad71221d60102c9d7f209150032d2
Juan Cassiano <juancassiano@hotmail.com>
2020-05-28 21:16:31 -0300

    Add signout context

--- src/hooks/AuthContext.tsx
+++ src/hooks/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, { useState, createContext, useCallback } from 'react';
+import React, { useContext, useState, createContext, useCallback } from 'react';
 import api from '../services/api';
 
 interface SignInCredentials {
@@ -9,6 +9,7 @@ interface SignInCredentials {
 interface AuthContextData {
   user: object;
   signIn(credentials: SignInCredentials): Promise<void>;
+  signOut(): void;
 }
 
 interface AuthState {
@@ -44,12 +45,30 @@ const AuthProvider: React.FC = ({ children}) =>{
     setData({ token, user });
   }, [])
 
+  const signOut = useCallback(() => {
+     localStorage.removeItem('@GoBarber:token');
+     localStorage.removeItem('@GoBarber:user');
+
+     setData({} as AuthState);
+
+  }, []);
+
   return(
-    <AuthContext.Provider value={{ user: data.user, signIn}}>
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut}}>
       {children}
     </AuthContext.Provider>
   )
 };
 
 
-export { AuthContext, AuthProvider };
+function useAuth(): AuthContextData {
+  const context = useContext(AuthContext);
+
+  if(!context){
+    throw new Error('useAuth must be used within an AuthProvider')
+  }
+
+  return context;
+}
+
+export { useAuth, AuthProvider };
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,11 +1,11 @@
-import React, { useRef, useCallback, useContext } from 'react';
+import React, { useRef, useCallback } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 import logoImg from '../../assets/logo.svg';
 
-import { AuthContext } from '../../context/AuthContext';
+import { useAuth } from '../../hooks/AuthContext';
 import getValidationErrors from '../../utils/getValidationErrors';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -20,9 +20,8 @@ interface SignInFormData {
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const { user, signIn } = useContext(AuthContext);
+  const { signIn } = useAuth();
 
-  console.log(user);
   const handleSubmit = useCallback(async (data: SignInFormData) => {
    try{
     formRef.current?.setErrors({});

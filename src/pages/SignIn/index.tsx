e882ac55627a542a5e73486161f1fc09ef8a0b3b
Juan Cassiano <juancassiano@hotmail.com>
2020-06-10 01:13:14 -0300

    Routes config

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -3,16 +3,17 @@ import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
-import logoImg from '../../assets/logo.svg';
+import { Link } from 'react-router-dom';
 
 import { useToast } from '../../hooks/ToastContext';
 import { useAuth } from '../../hooks/AuthContext';
 
+import logoImg from '../../assets/logo.svg';
 import getValidationErrors from '../../utils/getValidationErrors';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { Container, Content, Background } from './styles';
+import { Container, AnimationContainer, Content, Background } from './styles';
 
 interface SignInFormData {
   email: string;
@@ -50,6 +51,8 @@ const SignIn: React.FC = () => {
         const errors = getValidationErrors(err);
 
         formRef.current?.setErrors(errors);
+
+        return;
       }
       //Disparar um toast
       addToast({
@@ -64,23 +67,26 @@ const SignIn: React.FC = () => {
   return(
     <Container>
       <Content>
-        <img src={logoImg} alt="GoBarber" />
+        <AnimationContainer>>
+          <img src={logoImg} alt="GoBarber" />
 
-        <Form ref={formRef} onSubmit={handleSubmit}>
-          <h1>Faça seu logon</h1>
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Faça seu logon</h1>
 
-          <Input name="email" icon={FiMail} placeholder="E-mail" />
+            <Input name="email" icon={FiMail} placeholder="E-mail" />
 
-          <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
+            <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
 
-          <Button type="submit">Entrar</Button>
+            <Button type="submit">Entrar</Button>
 
-          <a href="forgot">Esqueci minha senha</a>
-        </Form>
+            <a href="forgot">Esqueci minha senha</a>
+          </Form>
 
-        <a href="login">
-          <FiLogIn />
-          Criar Conta</a>
+          <Link to="/signup">
+            <FiLogIn />
+            Criar Conta
+          </Link>
+        </AnimationContainer>
       </Content>
 
       <Background />
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -3,14 +3,14 @@ import { FiUser, FiArrowLeft, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 import { FormHandles } from '@unform/core';
+import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/logo.svg';
-
 import getValidationErrors from '../../utils/getValidationErrors';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { Container, Content, Background } from './styles';
+import { Container, AnimationContainer, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
@@ -44,23 +44,26 @@ const SignUp: React.FC = () => {
     <Background />
 
     <Content>
-      <img src={logoImg} alt="GoBarber" />
+      <AnimationContainer>
+        <img src={logoImg} alt="GoBarber" />
 
-      <Form ref={formRef} onSubmit={handleSubmit}>
-        <h1>Faça seu Cadastro</h1>
-        <Input name="name" icon={FiUser} placeholder="Nome" />
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Faça seu Cadastro</h1>
+          <Input name="name" icon={FiUser} placeholder="Nome" />
 
-        <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
 
-        <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
+          <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
 
-        <Button type="submit">Cadastrar</Button>
+          <Button type="submit">Cadastrar</Button>
 
-      </Form>
+        </Form>
 
-      <a href="login">
-        <FiArrowLeft />
-        Voltar para Logon</a>
+        <Link to="/">
+          <FiArrowLeft />
+          Voltar para Logon
+        </Link>
+      </AnimationContainer>
     </Content>
 
   </Container>

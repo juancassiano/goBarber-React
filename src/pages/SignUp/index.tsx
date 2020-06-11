0236469a77342709bb17b34c0d85278b514da7a4
Juan Cassiano <juancassiano@hotmail.com>
2020-06-10 23:04:54 -0300

    Login authenticate

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -3,19 +3,28 @@ import { FiUser, FiArrowLeft, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 import { FormHandles } from '@unform/core';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
+import api from '../../services/api';
 import logoImg from '../../assets/logo.svg';
 import getValidationErrors from '../../utils/getValidationErrors';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
-
+import { useToast } from '../../hooks/ToastContext';
 import { Container, AnimationContainer, Content, Background } from './styles';
+
+interface SignUpFormData {
+  name: string;
+  email: string;
+  password: string;
+}
 
 const SignUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+  const { addToast } = useToast();
+  const history = useHistory();
 
-  const handleSubmit = useCallback(async (data: object) => {
+  const handleSubmit = useCallback(async (data: SignUpFormData) => {
    try{
     formRef.current?.setErrors({});
 
@@ -31,12 +40,32 @@ const SignUp: React.FC = () => {
     await schema.validate(data,{
       abortEarly: false,
     });
-    }catch (err) {
-      const errors = getValidationErrors(err);
 
-      formRef.current?.setErrors(errors);
+    await api.post('/users', data);
+
+    history.push('/');
+
+    addToast({
+      type: 'success',
+      title: 'Cadastro realizado!',
+      description: 'Você já pode fazer seu logon no GoBarber!'
+    })
+    }catch (err) {
+      if(err instanceof Yup.ValidationError){
+        const errors = getValidationErrors(err);
+
+        formRef.current?.setErrors(errors);
+
+        return;
+      }
+      //Disparar um toast
+      addToast({
+        type: 'error',
+        title: 'Erro no cadastro',
+        description: 'Ocorreu um erro ao fazer o cadastro, tente novamente'
+      });
     }
-  },[]);
+  },[addToast, history]);
 
 
   return(
